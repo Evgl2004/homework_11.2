6b5d9ddf08ab96ced6b5651239c427ab78f2cd98
Evgl2004 <Evgl2004@yandex.ru>
2023-07-17 22:15:59 +0500

    Update str_func.py (описание функции)
    Внесено описание функции "str_upper"

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
-def str_upper(input_str):
+def str_upper(input_str):    """
+    Принимает на вход строку и возвращает её со всеми заглавными буквами.
+    :param input_str: Строка
+    :return:
+    """
     return input_str.upper()
 
